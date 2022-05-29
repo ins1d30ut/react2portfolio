eba414cdff50bfe88662445a887dac7c010f8aee
ins1d30ut <nfteebsv@protonmail.com>
2022-05-29 04:47:14 -0400

    commit css update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import ReactDOM from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
